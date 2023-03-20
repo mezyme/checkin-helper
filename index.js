fbf343b011c5304a5746d202139114849cd6faa6
ZhaoYue <zhaoyue1@lixiang.com>
2023-03-20 17:34:43 +0800

    feat:  puppeteer for juejin

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * @Author: ZhaoYue
  * @Date: 2022-10-27 15:10:15
  * @Description: 文件内容描述
- * @LastEditTime: 2022-12-19 16:41:48
+ * @LastEditTime: 2023-01-31 09:13:11
  * @LastEditors: ZhaoYue
  * @FilePath: /checkin-helper/index.js
  */
@@ -27,14 +27,72 @@ const v2free = require("./src/v2free/v2free")
  */
 
 // 每天8：10分执行任务 掘金签到
-schedule.scheduleJob("0 10 8 * * *", () => {
-   const nowTime = dayjs().format("YYYY-MM-DD HH:mm:ss");
-   console.log(`
-   ========== 现在时间: ${nowTime} ==========`);
-   juejin.juejinCheckIn();
-   v2free.v2CheckIn();
-});
+// schedule.scheduleJob("0 10 8 * * *", () => {
+//    const nowTime = dayjs().format("YYYY-MM-DD HH:mm:ss");
+//    console.log(`
+//    ========== 现在时间: ${nowTime} ==========`);
+//    juejin.juejinCheckIn();
+//    v2free.v2CheckIn();
+// });
 
 // 直接运行一次
-juejin.juejinCheckIn();
-v2free.v2CheckIn();
+// juejin.juejinCheckIn();
+// v2free.v2CheckIn();
+
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch({headless: false});
+  const page = await browser.newPage();
+
+  await page.goto('https://juejin.cn/');
+
+//   // Set screen size
+  await page.setViewport({width: 1400, height: 1200});
+
+  const loginElement = await page.waitForSelector('.login-button');
+  await loginElement.click();
+  const githubLogin = await page.waitForSelector('[title=GitHub]');
+  await githubLogin.click();
+  await page.waitForTimeout(6 * 1000);
+  const githubLoginPage = ( await browser.pages() )[2];//得到所有窗口使用列表索引得到新的窗口
+  // const newGithubLoginWindow = await browser.waitForTarget(
+  // target => target.url().includes('https://github.com/login'));
+  // const newPage = await newGithubLoginWindow;
+  await githubLoginPage.type('#login_field', 'mezyme@163.com');
+  await githubLoginPage.type('#password', '860926yue');
+  const githubloginElement = await githubLoginPage.waitForSelector('.js-sign-in-button');
+  await githubloginElement.click();
+  await page.waitForTimeout(10 * 1000);
+  // 去签到按钮
+  const checkInBtn = await page.waitForSelector('.signin-btn');
+  console.log('checkInBtn', checkInBtn);
+  await checkInBtn.click();
+  await page.waitForTimeout(3 * 1000);
+  const checkInNowBtn = await page.waitForSelector('.signin.btn');
+  console.log('checkInNowBtn', checkInNowBtn);
+  await checkInNowBtn.click();
+  await page.waitForTimeout(3 * 1000);
+  const chouJiangBtn = await page.waitForSelector('.btn-area .btn');
+  console.log('chouJiangBtn', chouJiangBtn);
+  await chouJiangBtn.click();
+
+//   // Type into search box
+//   await page.type('.search-box__input', 'automate beyond recorder');
+
+//   // Wait and click on first result
+//   const searchResultSelector = '.search-box__link';
+//   await page.waitForSelector(searchResultSelector);
+//   await page.click(searchResultSelector);
+
+//   // Localte the full title with a unique string
+//   const textSelector = await page.waitForSelector(
+//     'text/Customize and automate'
+//   );
+//   const fullTitle = await textSelector.evaluate(el => el.textContent);
+
+//   // Print the full title
+//   console.log('The title of this blog post is "%s".', fullTitle);
+
+//   await browser.close();
+})();
